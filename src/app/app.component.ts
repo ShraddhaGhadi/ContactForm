7d34dd564d99cfde474d128c9d10ac6aef242609
ShraddhaGhadi <shraddhaghadi311@gmail.com>
2024-12-05 15:54:24 +0530

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MarvellousComponent } from "./marvellous/marvellous.component";
+import { FormComponent } from "./form/form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MarvellousComponent, FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
